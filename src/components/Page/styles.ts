e2b4286908a5f0fcfaf4761d860da896a641a7e2
Lucashylario <lucashyfrotend@hotmail.com>
2022-04-07 11:49:44 -0300

    add imagens de background

--- src/components/Page/styles.ts
+++ src/components/Page/styles.ts
@@ -2,25 +2,47 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
     .colored:nth-child(1) {
-        background: #f1ffe7;
+        width: 100%;
+        background-image: url(https://images.unsplash.com/photo-1610470832703-95d40c3fad55?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=4000);
+        background-repeat: no-repeat;
+        background-size: cover;
+        background-position: top;
     }
     .colored:nth-child(2) {
-        background: #dfffd9;
+        background-image: url(https://images.unsplash.com/photo-1625062516780-2aa029398e58?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=4000);
+        background-repeat: no-repeat;
+        background-size: cover;
+        background-position: center;
     }
     .colored:nth-child(3) {
-        background: #cdfeca;
+        background-image: url(https://images.unsplash.com/photo-1538592116845-119a3974c958?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=4000);
+        background-repeat: no-repeat;
+        background-size: cover;
+        background-position: bottom;
     }
     .colored:nth-child(4) {
-        background: #bbfebb;
+        background-image: url(https://images.unsplash.com/photo-1600661653561-629509216228?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=4000);
+        background-repeat: no-repeat;
+        background-size: cover;
+        background-position: top;
     }
     .colored:nth-child(5) {
-        background: #a9fdac;
+        background-image: url(https://images.unsplash.com/photo-1597833063051-23a96f215ac1?ixlib=rb-2.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=4000);
+        background-repeat: no-repeat;
+        background-size: cover;
+        background-position: center;
     }
     .colored:nth-child(6) {
-        background: #90f29c;
+        background-image: url(https://images.unsplash.com/photo-1617931723605-dd4a47a618d2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=4000&q=80);
+        background-repeat: no-repeat;
+        background-size: cover;
+        background-position: center;
     }
     .colored:nth-child(7) {
-        background: #77e68c;
+        background-image: url(https://images.unsplash.com/photo-1625062516295-5169c87de09d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=4000&q=80);
+        background-repeat: no-repeat;
+        background-size: cover;
+        background-position: center;
     }
 `;
 
